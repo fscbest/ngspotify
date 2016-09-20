f376609baf044c27b2fcf13ec3d000e316fa6a53
fscbest <romangeyber@gmail.com>
2016-09-20 17:46:21 +0300

    Add empty Navbar and About and Bootswatch

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent, AboutComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
